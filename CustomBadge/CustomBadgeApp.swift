7a9de2e451609f3238cc73ccb87644cda4478ccd
Scott Crowder <scrowder@gmail.com>
2024-07-16 11:28:56 -0500

    Initial Commit

---
+++ CustomBadge/CustomBadgeApp.swift
@@ -0,0 +1,20 @@
+//
+// File: CustomBadgeApp.swift
+// Project: CustomBadge
+// 
+// Created by SCOTT CROWDER on 7/16/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct CustomBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
